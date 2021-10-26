df5a105f43d6d205bbd4110e82deba5c230e654a
Vipth <30879538+Vipth@users.noreply.github.com>
2021-10-26 09:05:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from random import choice
 
 class Game_Handler:
     """
-    Tool to assist in building card games. Has the classes:\n
-    `Dealer`\n
-    `player`
+    Tools to assist in building card games\n
+    Dealer Class:
+    ---
+    `Holds the deck of cards and deals with card disribution.`
+    Player Class:
+    ---
+    `Object that stores the player variables.`
     """
 
     class Dealer:
-        """Handles card distribution and organization."""
+        """
+        Holds the deck of cards and deals with card disribution.
+
+        Functions:
+        ---
+        `random_card()` - `Selects a random card within the deck.`
+        `deal_cards(player, cards=2)` - `Deals an amount, cards, to a player.`
+        """
         def __init__(self):
             self.Dealer = Game_Handler.Dealer
             # This is the deck of cards. 1 means the card is in the deck, 0 means the card is somewhere else.
@@ -77,7 +88,6 @@ class Game_Handler:
 
         def random_card(self):
             """Selects a random card from the deck."""
-
             # Creates a list containing the suites with cards left.
             active_suites = []
             for suite in self.Deck:
@@ -89,30 +99,36 @@ class Game_Handler:
             
             # Picks a suite from the list created above.
             self.suite = choice(active_suites)
-
+            
             # Creates a list of active cards from the suite chosen from above.
             active_cards_in_list = []
             for card in self.Deck[self.suite]:
                 if self.Deck[self.suite][card] == 1:
                     active_cards_in_list.append(card)
-
+            
             # Picks a card from the list created above.
             self.card = choice(active_cards_in_list)
-
+            
             # Removes card from Deck
             self.Deck[self.suite][self.card] = 0
-
+            
             # Returns the card that has been chosen.
             return self.suite, self.card
-
+            
         def deal_cards(self, player, cards=2):
             """Deals cards."""
             for _ in range(cards):
                 suite, card = self.random_card()
                 player.hand[suite].update({card: 1})
-
+            
     class Player:
-        """Object that stores the player variables."""
+        """
+        Object that stores the player variables.
+        Functions:
+        ---
+        `play_card(player, card: tuple)` - `Transfers a card to another player object.`\n
+        `get_cards()` - `Returns a neater dictionary only containing the cards in the player hand.`
+        """
         def __init__(self, player_name):
             self.name = player_name
             self.hand = {
@@ -121,16 +137,19 @@ class Game_Handler:
                 "Spades": {},
                 "Hearts": {}
             }
-
-        def play_card(self, player, card: tuple): # Tuple in format of (Suite, Card), Ex: ("Hearts", "Ace")
-            """Transfers a card to another player object."""
+            
+        def play_card(self, player, card: tuple):
+            """
+            Transfers a card to another player object.\n
+            Recieves the card in the tuple format: (Suite, Card). `Ex:("Hearts", "Ace")`.
+            """
             suite, card = card[0], card[1]
             if self.hand[suite][card] == 1:
                 self.hand[suite].update({card: 0})
                 player.hand[suite].update({card: 1})
             else:
                 print(f"Card does not exist within {self.name}'s hand.")
-
+            
         def get_cards(self):
             """Returns a neater dictionary only containing the cards in the player hand."""
             cards = dict()
@@ -141,12 +160,12 @@ class Game_Handler:
                     suite_total += self.hand[suite][card]
                 if suite_total > 0:
                     cards.update({suite: {}})
-
+            
             # Updates the active cards to the proper suite in the cards dictionary.
             for suite in self.hand:
                 for card in self.hand[suite]:
                     if self.hand[suite][card] == 1:
                         cards[suite].update({card: 1})
-
+            
             # Return the updated cards dictionary.
             return cards
